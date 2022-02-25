d177d37f5354aae8401c0d590ea930b0394c0201
azxazx321 <30621599+azxazx321@users.noreply.github.com>
2022-02-25 14:56:56 +1100

    header static web paged completed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import './index.css'
+import './index.less'
 
 export default function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+      
+        <div className='header-top'>
+          <span>Hi,Admin</span>
+          <button>Quit</button>
+          </div>
+        <div className='header-bottom'>
+          <div className='header-bottom-left'>Index</div>
+          <div className='header-bottom-right'>
+            <span>29348930243029</span>
+            <img src="" alt="" />
+             <span>Clear</span>
+          </div>
+        </div>
+      
+    </div>
   )
 }
