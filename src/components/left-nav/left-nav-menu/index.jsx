de5e816f543b769a6e9a4d4d47668eaede3f3e12
luke <305867377@qq.com>
2022-03-19 15:55:53 +1100

    fix left-nav product menu bug

--- src/components/left-nav/left-nav-menu/index.jsx
+++ src/components/left-nav/left-nav-menu/index.jsx
@@ -17,32 +17,30 @@ const { SubMenu } = Menu;
 
   export default function LeftNavMenu() {
     const [collapsed,setCollapsed] = useState(false)
-    //const [nodes, setNode] = useState()
     let location = useLocation();
-    console.log('111 :>> ',location);
     let currentPathname = location.pathname
-
     
-    //const useComponentWillMount = () => getMenuNodes(menuList)
+    const getCurrentPath= () => {
+      console.log(currentPathname)
+       if(currentPathname.indexOf('/admin/product')===0){
+        currentPathname = '/admin/product'
+      }
+       return currentPathname
+    }
+    
 
     const getMenuNodes = (menuList) => {
       
       return menuList.reduce( (pre,item) => {
         if(!item.children){
            const newItem = (<Menu.Item key={item.key} icon={<ContainerOutlined />}>
-             {/* Link 里面可以利用state将pathname传递给location的state，然后用useLocation 取出*/}
              <Link to={item.key} >{item.title}</Link> 
           </Menu.Item>);
           pre = [...pre,newItem]
-          //pre.push(newItem)
         }else {
-          //console.log('getsubMunuNode!!!!!',item.key)
           const newItem = (<SubMenu key={item.key} icon={<MailOutlined />} title={item.title}> 
             {getMenuNodes(item.children)}
       </SubMenu>)
-          // pre.push(
-          //   newItem
-          // )
           pre = [...pre,newItem]
          
         }
@@ -78,13 +76,13 @@ const { SubMenu } = Menu;
     return (
       <div style={{ width: 200 }}>
         
-          {/* 只要调用函数进行执行，都必须加括号。此处加了括号，可能就为固定值，所以不加 */}
+          {/* 只要调用函数进行执行，都必须加括号。此处加了括号，就为固定值，所以不加 */}
           <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
             {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
           </Button>
           <Menu
             defaultSelectedKeys={['home']} //初始选中的菜单项 key 数组	
-            selectedKeys={currentPathname} //当前选中的菜单项 key 数组	
+            selectedKeys={getCurrentPath()} //当前选中的菜单项 key 数组	
             defaultOpenKeys={getOpenKey()} //初始展开的 SubMenu 菜单项 key 数组	
             mode="inline"
             theme="dark"
